e5dbbc488f7e307c6039cae7cd96033c46561ed6
Anthony Balaine <abalaine@nexient.com>
2022-12-15 18:10:58 -0500

    initial

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import Navbar from '../Components/Navbar.svelte';
+	import Footer from '../Components/Footer.svelte';
+</script>
+
+<Navbar />
+
+<main>
+	<slot />
+</main>
+
+<Footer />
---
+++ src/routes/+page.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+	import Home from '../Components/Home.svelte';
+</script>
+
+<Home />
