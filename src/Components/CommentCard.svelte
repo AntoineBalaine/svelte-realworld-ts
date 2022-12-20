8ff62f7305e1f081d339198944e8a2dc57ea73f0
Anthony Balaine <abalaine@nexient.com>
2022-12-20 10:24:24 -0500

    setup article page
    The articles page is also supposed to have some cards to display
    readers’ comments.
    I separated the CommentCard into its own component - since it will be
    used only later down the line

---
+++ src/Components/CommentCard.svelte
@@ -0,0 +1,27 @@
+<div class="row">
+	<div class="col-xs-12 col-md-8 offset-md-2">
+		<form class="card comment-form">
+			<div class="card-block">
+				<textarea class="form-control" placeholder="Write a comment..." rows="3" />
+			</div>
+			<div class="card-footer">
+				<img src="http://i.imgur.com/Qr71crq.jpg" class="comment-author-img" />
+				<button class="btn btn-sm btn-primary">Post Comment</button>
+			</div>
+		</form>
+	</div>
+</div>
+
+<div class="card">
+	<div class="card-block">
+		<p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+	</div>
+	<div class="card-footer">
+		<a href="" class="comment-author">
+			<img src="http://i.imgur.com/Qr71crq.jpg" class="comment-author-img" />
+		</a>
+		&nbsp;
+		<a href="" class="comment-author">Jacob Schmidt</a>
+		<span class="date-posted">Dec 29th</span>
+	</div>
+</div>
---
+++ src/routes/article/+page.svelte
@@ -0,0 +1,66 @@
+<script lang="ts">
+	import { store } from "$store";
+	import type ApiTypes from "$ApiTypes";
+	export let article: ApiTypes.components["schemas"]["Article"];
+	store.subscribe((data) => (article = data.articles[0]));
+	const { author, updatedAt, favoritesCount, title, body, description } = article;
+</script>
+
+<div class="article-page">
+	<div class="banner">
+		<div class="container">
+			<h1>{title}</h1>
+
+			<div class="article-meta">
+				<a href="/{author.username}"><img src={author.image} alt="author profile" /></a>
+				<div class="info">
+					<a href="/{author.username}" class="author">{author.username}</a>
+					<span class="date">{updatedAt}</span>
+				</div>
+				<button class="btn btn-sm btn-outline-secondary">
+					<i class="ion-plus-round" />
+					&nbsp; Follow {author.username} <span class="counter">({favoritesCount})</span>
+				</button>
+				&nbsp;&nbsp;
+				<button class="btn btn-sm btn-outline-primary">
+					<i class="ion-heart" />
+					&nbsp; Favorite Post <span class="counter">({favoritesCount})</span>
+				</button>
+			</div>
+		</div>
+	</div>
+
+	<div class="container page">
+		<div class="row article-content">
+			<div class="col-md-12">
+				<p>
+					{description}
+				</p>
+				<!-- 				<h2 id="introducing-ionic">{slug}</h2> -->
+				<p>{body}</p>
+			</div>
+		</div>
+
+		<hr />
+
+		<div class="article-actions">
+			<div class="article-meta">
+				<a href="profile.html"><img src={author.image} alt="author profile" /></a>
+				<div class="info">
+					<a href="/{author.username}" class="author">{author.username}</a>
+					<span class="date">{updatedAt}</span>
+				</div>
+
+				<button class="btn btn-sm btn-outline-secondary">
+					<i class="ion-plus-round" />
+					&nbsp; Follow {author.username}
+				</button>
+				&nbsp;
+				<button class="btn btn-sm btn-outline-primary">
+					<i class="ion-heart" />
+					&nbsp; Favorite Post <span class="counter">({favoritesCount})</span>
+				</button>
+			</div>
+		</div>
+	</div>
+</div>
